3fdfacce20a53c5331327fec2e640c899d6c72e7
Andrew Hird <andrew.james.hird@gmail.com>
2016-12-01 11:04:39 +1300

    image dummy data

--- src/reducers/images.js
+++ src/reducers/images.js
@@ -1,9 +1,16 @@
-const images = (state = [], action) => {
+const images = (state = [
+  {
+    id: 0,
+    image: 'https://www.royalcanin.com/~/media/Royal-Canin/Product-Categories/cat-adult-landing-hero.ashx'
+  }, {
+    id: 1,
+    image: 'https://www.royalcanin.com/~/media/Royal-Canin/Product-Categories/dog-medium-landing-hero.ashx'
+  }
+], action) => {
   switch (action.type) {
     case 'ADD_IMAGE':
       return [
-        ...state,
-        {
+        ...state, {
           id: action.id,
           image: action.image
         }
